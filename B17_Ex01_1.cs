be813c922225b5e0b256b4b1985386c109892a15
liorokach <liorokach@gmail.com>
2017-04-13 19:19:27 +0300

    q1 and q5
    q1 and q5 beta's

---
+++ B17_Ex01_1.cs
@@ -0,0 +1,103 @@
+﻿// lior rokach
+using System.Text;
+
+public class B17_Ex01_1
+{
+     public static class Constants
+     {
+          public const int k_NumOfInputs = 3; // number of input from user
+     }
+
+     public static void Main()  ////this method is the main
+     {
+          int[] decNums = new int[Constants.k_NumOfInputs];
+          int countDigitsOfTheBin = 0, countUp = 0, countDown = 0;
+          float avgValueFromInput, avgDigInBinNum;
+          string binInStr, inputNumByStr; // input by user;
+          System.Console.WriteLine("Please enter 3 positive numbers with 3 digits each:");
+          for (int i = 0; i < Constants.k_NumOfInputs; i++)
+          {
+               inputNumByStr = System.Console.ReadLine();// get input from user
+               int.TryParse(inputNumByStr, out decNums[i]);// check if input is number
+               while (decNums[i] == 0 || inputNumByStr.Length != 3)// checking for unvalid input and loop until get valid one
+               {
+                    i = countUp = countDown = 0;// zeroing 
+                    System.Console.WriteLine("The input you entered is invalid. Please try again.");// inform the user that his input invalid
+                    inputNumByStr = System.Console.ReadLine();
+                    int.TryParse(inputNumByStr, out decNums[i]);
+               }
+               countUp += IsIncreasing(inputNumByStr.ToString());// increment if the number's digits are increasing
+               countDown += IsIncreasing(ReverseString(inputNumByStr).ToString());// increment if the number's digits are decreasing
+          }
+
+          System.Console.Write("The binary numbers are: ");
+          for (int i = 0; i < Constants.k_NumOfInputs; i++)
+          {
+               binInStr = ConvertToBinary(decNums[i]);// convert decimal to binary and print the binary value
+               System.Console.Write(binInStr);
+               countDigitsOfTheBin += binInStr.Length;// add the num of digits of the binary number
+               System.Console.Write(i == Constants.k_NumOfInputs - 1 ? "\n" : " ");// print 'space' ,  or 'endline' at the last number to print 
+          }
+
+          avgValueFromInput = CalculateAvgValue(decNums);// calculte the average value of a input from user number(decimal)
+          avgDigInBinNum = (float)countDigitsOfTheBin / Constants.k_NumOfInputs;// calculate the number of digits in the average binary number
+          PrintResult(countUp, countDown, avgDigInBinNum, avgValueFromInput);// printing the result format
+     }
+
+     public static void PrintResult(int AscendingCount, int DescendingCount, float AvgNumOfDigInBin, float AvgOfInsertNums)
+     {
+          System.Console.WriteLine(string.Format("There are {0} numbers which are an ascending series and {1} which are descending.", AscendingCount, DescendingCount));
+          System.Console.WriteLine("The average number of digits in the binary number is " + AvgNumOfDigInBin.ToString());
+          System.Console.WriteLine("The general average of the inserted numbers is " + AvgOfInsertNums.ToString());
+     }
+
+     public static float CalculateAvgValue(int[] i_IntArray)
+    {
+        int sumOfNums = 0;
+        for (int i = 0; i < i_IntArray.Length; i++)
+        {
+            sumOfNums += i_IntArray[i];
+        }
+
+        return (float)sumOfNums / i_IntArray.Length;
+    }
+
+     public static StringBuilder ReverseString(string i_Str)
+    {
+        StringBuilder reverseStr = new StringBuilder(i_Str.Length);
+        for (int i = i_Str.Length - 1; i >= 0; i--)
+        {
+            reverseStr.Append(i_Str[i]);
+        }
+
+        return reverseStr;
+    }
+
+     public static int IsIncreasing(string i_TextNumber)
+    {
+        char previous = (char)('0' - 1);
+        foreach (char c in i_TextNumber)
+        {
+            if (c <= previous)
+            {
+                return 0;
+            }
+
+            previous = c;
+        }
+
+        return 1;
+    }
+
+     public static string ConvertToBinary(int i_DecNum) ////this method convert dec to binary
+    {
+        string binaryStr = string.Empty;
+        while (i_DecNum > 0)
+        {
+            binaryStr = (i_DecNum % 2).ToString() + binaryStr;
+            i_DecNum /= 2;
+        }
+
+        return binaryStr;
+    }
+}
---
+++ B17_Ex01_5.cs
@@ -0,0 +1,90 @@
+﻿public class B17_Ex01_5
+{
+     public static class Constants
+     {
+          public const int k_InputLen = 8; // number of input from user
+     }
+
+     public static void Main()  ////this method is the main
+     {
+          int userNumber, minDigVal, maxDigVal, aboveUnityDig, belowUnityDig;
+          userNumber = GetEightDigitsNumberInput();
+          maxDigVal = GetMaxDigit(userNumber);
+          minDigVal = GetMinDigit(userNumber);
+          aboveUnityDig = GetAboveUnityDig(userNumber);
+          belowUnityDig = GetBelowUnityDig(userNumber);
+          PrintDetailsFormat(maxDigVal, minDigVal, aboveUnityDig, belowUnityDig);
+     }
+
+     public static void PrintDetailsFormat(int maxDigVal, int minDigVal, int aboveUnityDig, int belowUnityDig)
+     {
+          System.Console.WriteLine(string.Format("The max digit in the number is {0} and the min digit is {1}.", maxDigVal, minDigVal));
+          System.Console.WriteLine(string.Format("The number of digits above the unity is {0} and {1} are below the unity.", aboveUnityDig, belowUnityDig));
+     }
+
+     public static int GetEightDigitsNumberInput()
+     {
+          int userNumber;
+          System.Console.WriteLine("Please enter integer positive number with 8 digits:");
+          string inputNumByStr = System.Console.ReadLine(); // get input from user
+          int.TryParse(inputNumByStr, out userNumber);
+          while (userNumber == 0 || inputNumByStr.Length != Constants.k_InputLen)
+          {
+               System.Console.WriteLine("Invalid input, please try again.");
+               inputNumByStr = System.Console.ReadLine(); // get input from user
+               int.TryParse(inputNumByStr, out userNumber);
+          }
+
+          return userNumber;
+     }
+
+     public static int GetAboveUnityDig(int i_number)
+     {
+          int unityDigit = i_number % 10, countAbove = 0;
+          i_number /= 10;
+          while (i_number > 0)
+          {
+               countAbove = i_number % 10 > unityDigit ? countAbove + 1 : countAbove;
+               i_number /= 10;
+          }
+
+          return countAbove;
+     }
+
+     public static int GetBelowUnityDig(int i_number)
+     {
+          int unityDigit = i_number % 10, countBelow = 0;
+          i_number /= 10;
+          while (i_number > 0)
+          {
+               countBelow = i_number % 10 < unityDigit ? countBelow + 1 : countBelow;
+               i_number /= 10;
+          }
+
+          return countBelow;
+     }
+
+     public static int GetMaxDigit(int i_Num)
+     {
+          int maxDig = 0;
+          while (i_Num != 0) 
+          {
+               maxDig = maxDig < i_Num % 10 ? i_Num % 10 : maxDig;
+               i_Num /= 10;
+          }
+
+          return maxDig;
+     }
+
+     public static int GetMinDigit(int i_Num)
+     {
+          int minDig = 10;
+          while (i_Num != 0)
+          {
+               minDig = minDig > i_Num % 10 ? i_Num % 10 : minDig;
+               i_Num /= 10;
+          }
+
+          return minDig;
+     }
+} 
