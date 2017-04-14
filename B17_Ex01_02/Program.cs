7722040991d8f02c113e91b9b17dc36361da12d2
oznasi1 <oznasi1@gmail.com>
2017-04-14 21:09:23 +0300

    Add files via upload

---
+++ B17_Ex01_02/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace B17_Ex01_02
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            SandClockHightFive(0);
+        }
+
+        public static void SandClockHightFive(int numSpaces)
+        {
+            string spaces = string.Empty;
+            for (int i = 0; i < numSpaces; i++)
+            {
+                spaces += " ";
+            }
+
+            Console.WriteLine(string.Format("{5}{0}\n {5}{1}\n {5} {2}\n{5} {3}\n{5}{4}", "*****", "***", "*", "***", "*****", spaces));
+        }
+    }
+}
