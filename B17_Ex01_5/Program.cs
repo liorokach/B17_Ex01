0c9af645b3646d413daa6ab543848f9f36399dcf
liorokach <liorokach@gmail.com>
2017-04-15 16:27:01 +0300

    documentary and bug fixes

--- B17_Ex01_5/Program.cs
+++ B17_Ex01_5/Program.cs
@@ -1,10 +1,14 @@
-﻿namespace B17_Ex01_5
+﻿////Ex01_Q5
+////Lior Rokach & Oz Nasi 15.04.17
+////This program get 8 digits positive integer from user and found and print to screen the max digit and min digit, 
+////Additionally the program prints how many digits are bigger than the unity digit and how much smaller
+namespace B17_Ex01_5
 {
      public class Program
      {
           public static class Constants
           {
-               public const int k_InputLen = 8; // number of input from user
+               public const int k_InputLen = 8; // number of digits input from user
           }
 
           public static void Main()
@@ -14,22 +18,24 @@
 
           public static void Ex01Q5Manager()
           {
-               int userNumber, minDigVal, maxDigVal, aboveUnityDig, belowUnityDig;
-               userNumber = GetEightDigitsNumberInput();
-               maxDigVal = GetMaxDigit(userNumber);
-               minDigVal = GetMinDigit(userNumber);
-               aboveUnityDig = GetAboveUnityDig(userNumber);
-               belowUnityDig = GetBelowUnityDig(userNumber);
-               PrintDetailsFormat(maxDigVal, minDigVal, aboveUnityDig, belowUnityDig);
+               int aboveUnityDig, belowUnityDig;
+               string userNumberStr;
+               char minDigVal, maxDigVal;
+               userNumberStr = GetEightDigitsNumberInput();
+               maxDigVal = GetMaxDigit(userNumberStr);
+               minDigVal = GetMinDigit(userNumberStr);
+               aboveUnityDig = GetAboveUnityDig(userNumberStr);
+               belowUnityDig = GetBelowUnityDig(userNumberStr);
+               PrintResult(maxDigVal, minDigVal, aboveUnityDig, belowUnityDig);
           }
 
-          public static void PrintDetailsFormat(int maxDigVal, int minDigVal, int aboveUnityDig, int belowUnityDig)
+          public static void PrintResult(char i_maxDigVal, char i_minDigVal, int i_aboveUnityDig, int i_belowUnityDig)
           {
-               System.Console.WriteLine(string.Format("The maximum digit in the number is {0} and the minimum digit is {1}.", maxDigVal, minDigVal));
-               System.Console.WriteLine(string.Format("The number of digits above the unity is {0} and {1} is below the unity.", aboveUnityDig, belowUnityDig));
+               System.Console.WriteLine(string.Format("The maximum digit in the number is {0} and the minimum digit is {1}.", i_maxDigVal, i_minDigVal));
+               System.Console.WriteLine(string.Format("The number of digits above the unity is {0} and {1} is below the unity.", i_aboveUnityDig, i_belowUnityDig));
           }
 
-          public static int GetEightDigitsNumberInput()
+          public static string GetEightDigitsNumberInput()
           {
                int userNumber;
                System.Console.WriteLine("Please enter integer positive number with 8 digits:");
@@ -42,62 +48,60 @@
                     int.TryParse(inputNumByStr, out userNumber);
                }
 
-               return userNumber;
+               return inputNumByStr;
           }
 
-          public static int GetAboveUnityDig(int i_number)
+          public static int GetAboveUnityDig(string i_numberStr)
           {
-               int unityDigit = i_number % 10, countAbove = 0;
-               i_number /= 10;
-               while (i_number > 0)
+               int countAbove = 0;
+               for (int i = 0; i < Constants.k_InputLen - 1; i++) 
                {
-                    if(i_number % 10 > unityDigit)
+                    if (i_numberStr[i] > i_numberStr[Constants.k_InputLen - 1])
                     {
                          countAbove++;
                     }
-
-                    i_number /= 10;
                }
 
                return countAbove;
           }
 
-          public static int GetBelowUnityDig(int i_number)
+          public static int GetBelowUnityDig(string i_numberStr)
           {
-               int unityDigit = i_number % 10, countBelow = 0;
-               i_number /= 10;
-               while (i_number > 0)
+               int countBelow = 0;
+               for (int i = 0; i < Constants.k_InputLen - 1; i++)
                {
-                    if (i_number % 10 < unityDigit)
+                    if (i_numberStr[i] < i_numberStr[Constants.k_InputLen - 1])
                     {
                          countBelow++;
                     }
-
-                    i_number /= 10;
                }
 
                return countBelow;
           }
 
-          public static int GetMaxDigit(int i_Num)
+          public static char GetMaxDigit(string i_NumStr)
           {
-               int maxDig = 0;
-               while (i_Num != 0)
+               char maxDig = '0';
+               for (int i = 0; i < Constants.k_InputLen; i++)
                {
-                    maxDig = System.Math.Max(maxDig, i_Num % 10);
-                    i_Num /= 10;
+                    if (maxDig < i_NumStr[i])
+                    {
+                         maxDig = i_NumStr[i];
+                    }
                }
 
                return maxDig;
           }
 
-          public static int GetMinDigit(int i_Num)
+          public static char GetMinDigit(string i_NumStr)
           {
-               int minDig = 10;
-               while (i_Num != 0)
+               char minDig = '9';
+               for (int i = 0; i < Constants.k_InputLen; i++)
                {
-                    minDig = System.Math.Min(minDig, i_Num % 10);
-                    i_Num /= 10;
+                    if (minDig > i_NumStr[i])
+                    {
+                         minDig = i_NumStr[i];
+                    }
                }
 
                return minDig;
