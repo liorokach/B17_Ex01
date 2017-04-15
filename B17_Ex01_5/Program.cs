393c9a9d22bbee0930f9eb7895f421b6ab18579d
liorokach <liorokach@gmail.com>
2017-04-15 19:15:02 +0300

    documentary

--- B17_Ex01_5/Program.cs
+++ B17_Ex01_5/Program.cs
@@ -16,6 +16,7 @@ namespace B17_Ex01_5
                Ex01Q5Manager();
           }
 
+          /*this function get 8 digits number from user and check details about this number(max/min digit, how many digits bigger/smaller than unity digit) and print the result*/
           public static void Ex01Q5Manager()
           {
                int aboveUnityDig, belowUnityDig;
@@ -24,17 +25,19 @@ namespace B17_Ex01_5
                userNumberStr = GetEightDigitsNumberInput();
                maxDigVal = GetMaxDigit(userNumberStr);
                minDigVal = GetMinDigit(userNumberStr);
-               aboveUnityDig = GetAboveUnityDig(userNumberStr);
-               belowUnityDig = GetBelowUnityDig(userNumberStr);
+               aboveUnityDig = CountAboveUnityDig(userNumberStr);
+               belowUnityDig = CountBelowUnityDig(userNumberStr);
                PrintResult(maxDigVal, minDigVal, aboveUnityDig, belowUnityDig);
           }
 
+          /*this function get 2 char represent max/min digit and 2 integer represent number of digits above/below the unity and print them to the user */
           public static void PrintResult(char i_maxDigVal, char i_minDigVal, int i_aboveUnityDig, int i_belowUnityDig)
           {
                System.Console.WriteLine(string.Format("The maximum digit in the number is {0} and the minimum digit is {1}.", i_maxDigVal, i_minDigVal));
                System.Console.WriteLine(string.Format("The number of digits above the unity is {0} and {1} is below the unity.", i_aboveUnityDig, i_belowUnityDig));
           }
 
+          /*this function get from the user eight digits positive integer(check validation) and return the valid input as string*/
           public static string GetEightDigitsNumberInput()
           {
                int userNumber;
@@ -51,7 +54,8 @@ namespace B17_Ex01_5
                return inputNumByStr;
           }
 
-          public static int GetAboveUnityDig(string i_numberStr)
+          /*this function count how many digits are bigger than the unity digits and return the counter*/
+          public static int CountAboveUnityDig(string i_numberStr)
           {
                int countAbove = 0;
                for (int i = 0; i < Constants.k_InputLen - 1; i++) 
@@ -65,7 +69,8 @@ namespace B17_Ex01_5
                return countAbove;
           }
 
-          public static int GetBelowUnityDig(string i_numberStr)
+          /*this function count how many digits are smaller than the unity digits and return the counter*/
+          public static int CountBelowUnityDig(string i_numberStr)
           {
                int countBelow = 0;
                for (int i = 0; i < Constants.k_InputLen - 1; i++)
@@ -79,29 +84,24 @@ namespace B17_Ex01_5
                return countBelow;
           }
 
+          /*this function get string and return the biggest char value of the string number*/
           public static char GetMaxDigit(string i_NumStr)
           {
                char maxDig = '0';
                for (int i = 0; i < Constants.k_InputLen; i++)
                {
-                    if (maxDig < i_NumStr[i])
-                    {
-                         maxDig = i_NumStr[i];
-                    }
+                    maxDig = (char)(System.Math.Max(maxDig - '0', i_NumStr[i] - '0')+'0');
                }
 
                return maxDig;
           }
-
+          /*this function get string and return the smallest char value of the string number*/
           public static char GetMinDigit(string i_NumStr)
           {
                char minDig = '9';
                for (int i = 0; i < Constants.k_InputLen; i++)
                {
-                    if (minDig > i_NumStr[i])
-                    {
-                         minDig = i_NumStr[i];
-                    }
+                    minDig = (char)(System.Math.Min(minDig - '0', i_NumStr[i] - '0')+'0');
                }
 
                return minDig;
