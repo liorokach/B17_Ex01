96afaa97d2019aec7834ac59a55bc2eb21827b73
liorokach <liorokach@gmail.com>
2017-04-15 18:10:12 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿////Ex01Q1
 ////Lior Rokach & Oz Nasi 15.04.17
-////This program get 3 positive integers from user and convert the numbers to their binary value and print/
+////This program get 3 positive integers from user and convert the numbers to their binary value and print
 ////Additionally it calculate and print the average digits of binary number, the average value of the integer input number and how many numbers are ascending and decreasing 
 using System.Text;
 
@@ -18,18 +18,20 @@ namespace B17_Ex01_1
                Ex01Q1Manager();
           }
 
+          /*this function manage the exercise*/
           public static void Ex01Q1Manager()
           {
                int[] decNums = new int[Constants.k_NumOfInputs];
-               int countDigitsOfTheBin = 0, countAscending = 0, countDecreasing = 0;
+               int countDigitsOfTheBin = 0, countAscending = 0, countDescending = 0;
                float avgValueFromInput, avgDigInBinNum;
-               decNums = GetValidInput(ref countAscending, ref countDecreasing);
+               decNums = GetValidInputAndCheckSeries(ref countAscending, ref countDescending);
                countDigitsOfTheBin = PrintBinaryNumbers(decNums);
                avgValueFromInput = CalculateAvgValue(decNums); // calculte the average value of a input from user number(decimal)
                avgDigInBinNum = (float)countDigitsOfTheBin / Constants.k_NumOfInputs; // calculate the number of digits in the average binary number
-               PrintResult(countAscending, countDecreasing, avgDigInBinNum, avgValueFromInput); // printing the result format
+               PrintResult(countAscending, countDescending, avgDigInBinNum, avgValueFromInput); // printing the result format
           }
 
+          /*this function get integer array and print the binary value of the integer arrray decimal numbers,additionally the function count the number of digits of all the binary numbers and return that */
           public static int PrintBinaryNumbers(int[] i_decNumbers)
           {
                string binaryStr;
@@ -46,7 +48,8 @@ namespace B17_Ex01_1
                return countDigits;
           }
 
-          public static int[] GetValidInput(ref int o_CountUp, ref int o_CountDown)
+          /*this function get input from user and check if it valid while check if the input is series(ascending\descending), return the input as integer array + by ref counter of ascending and descending number */
+          public static int[] GetValidInputAndCheckSeries(ref int o_CountUp, ref int o_CountDown)
           {
                int[] decNumberArr = new int[Constants.k_NumOfInputs];
                string inputNumByStr;
@@ -70,6 +73,7 @@ namespace B17_Ex01_1
                return decNumberArr;
           }
 
+          /*this function print the result(how many numbers are ascending/descending series ,the average digits of a binary number,the average of the input numbers*/
           public static void PrintResult(int i_AscendingCount, int i_DescendingCount, float i_AvgNumOfDigInBin, float i_AvgOfInsertNums)
           {
                System.Console.WriteLine("There are {0} numbers which are an ascending series and {1} which are descending.", i_AscendingCount, i_DescendingCount);
@@ -77,6 +81,7 @@ namespace B17_Ex01_1
                System.Console.WriteLine("The general average of the inserted numbers is " + i_AvgOfInsertNums.ToString());
           }
 
+          /*this function get an integer array and calculate+return the average value of the numbers*/
           public static float CalculateAvgValue(int[] i_IntArray)
           {
                int sumOfNums = 0;
@@ -88,6 +93,7 @@ namespace B17_Ex01_1
                return (float)sumOfNums / i_IntArray.Length;
           }
 
+          /*this function get a string and reverse it and return the reverse string as stringbuilder*/
           public static StringBuilder ReverseString(string i_StrToReverse)
           {
                StringBuilder reverseStr = new StringBuilder(i_StrToReverse.Length);
